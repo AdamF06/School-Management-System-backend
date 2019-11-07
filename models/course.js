e7330137034a74a1d106fd2d8d31df887c3f7db6
AdamF06 <qq2841864141@gmail.com>
2019-11-07 15:05:17 +1100

    FEAT-003 course schema

--- models/course.js
+++ models/course.js
@@ -1,4 +1,44 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const 
+const courseSchema = new Schema({
+    course_ID = { type: String, unique: true },
+    state: String,//on going, complete, on apply...
+    name: String,
+    course_code: String,
+    number_of_students: Number,
+    prerequest_knowledge: [
+        { knowledge: String }
+    ],
+    course_length: String,
+    teaching_method: String,
+    city: String,
+    course_objective: String,
+    level: String,
+    start_apply_date: Date,
+    tution: Number, //学费
+    complete_date: Date,
+    teachers: [
+        { teacher_ID: String },
+    ],
+    address: [
+        { address_1: String },
+        { address_2: String }
+    ],
+    thumbnall: Number,
+
+    assignment: [
+        { assignment_name: String }
+    ],//这个ass没有ID,每个学生的ass通过ass_ID查看
+    project: [
+        {
+            project_name: String,
+            start_date: Date,
+            end_date: Date,
+            marks: Number,
+            description: String,
+        }
+    ]
+})
+
+module.exports = mongoose.model('Course', courseSchema)
